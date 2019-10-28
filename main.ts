ef14d7106188f1e6c1c181e87b23268bf1f47092
tsunyi <tsunyi@users.noreply.github.com>
2019-10-28 18:22:28 +0800

    Add readDistance Block

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
- 
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=190 weight=100 icon="\uf1ec" block="Ovobot Modules"
+namespace ovobotModules {
+    const SONAR_ADDRESS = 0x52
+
+    function sonicEnable() {
+        pins.i2cWriteRegister(SONAR_ADDRESS, 0x00, 0x01);
+    }
+
+    /**
+     * TODO: 获取超声波传感器与前方障碍物的距离函数。
+     */
+    //% block weight=50
+    export function readDistance(): number {
+        sonicEnable();
+
+        let sonarVal = pins.i2cReadRegister(SONAR_ADDRESS, 0x01, NumberFormat.Int16LE);
+        let distance = sonarVal / 29;
+
+        return distance;
+    }
+}

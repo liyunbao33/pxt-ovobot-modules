eb851d7cb1596ee54af3c9056954cf72296f3e67
tsunyi <tsunyi@users.noreply.github.com>
2020-03-09 17:55:51 +0800

    add control motor output block

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
 /**
  * Provides access to basic micro:bit functionality.
  */
+
+enum ModuleIndex {
+    //% block="module1"
+    Module1,
+    //% block="module2"
+    Module2,
+    //% block="module3"
+    Module3,
+    //% block="module4"
+    Module4
+}
+
 //% color=190 weight=100 icon="\uf1ec" block="Ovobot Modules"
 namespace ovobotModules {
     const SONAR_ADDRESS = 0x52
+    const MOTOR_ADDRESS = 0x64
 
     function sonicEnable() {
         pins.i2cWriteRegister(SONAR_ADDRESS, 0x00, 0x01);
@@ -20,5 +33,19 @@ namespace ovobotModules {
         let distance = sonarVal / 29;
 
         return distance;
+    }
+
+    /**
+     * TODO: 控制马达PWM输出。
+     */
+    //% blockId=control_motor_output block="control motor %module output %speed"
+    //% weight=65
+    export function controlMotorOutput(module: ModuleIndex, speed: number) {
+        let buf = pins.createBuffer(8);
+        buf[0] = 0x00;
+        buf[1] = speed > 0 ? 0 : 1;
+        buf[2] = Math.abs(speed)
+
+        pins.i2cWriteBuffer(MOTOR_ADDRESS + module, buf);
     }
 }

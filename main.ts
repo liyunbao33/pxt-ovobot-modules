7f79273f56d491706079ea496bc12005406ea6a4
liyunbao <liyunbao33@163.com>
2020-08-26 20:31:31 +0800

    修改灯带bug

--- main.ts
+++ main.ts
@@ -211,21 +211,21 @@ namespace ovobotModules {
     //% blockId=control_leds_output block="control neopixels %index color %color"
     //% weight=65
     export function controlNeopixels(index: LedIndex, color: Color) { 
-        let buf = pins.createBuffer(17);
+        let buf = pins.createBuffer(20);
         let startPos;
         buf[0] = 0;
         buf[1] = 1;
         if (index == 0) {
-            for (let i = 2; i < 15; i += 3) {
+            for (let i = 5; i < 18; i += 3) {
                 buf[i] = ((selectColors[color] >> 8) & 0xff) / lowBright;
                 buf[i + 1] = ((selectColors[color] >> 16) & 0xff) / lowBright;
                 buf[i + 2] = (selectColors[color] & 0xff) / lowBright;
             }
-        } else { 
-            startPos = 2 + 3 * (index - 1);
-            buf[startPos] = 0xFF;
-            buf[startPos + 1] = 0;
-            buf[startPos + 2] = 0;
+        } else {
+            startPos = 2 + 3 * index;
+            buf[startPos] = ((selectColors[color] >> 8) & 0xff) / lowBright;
+            buf[startPos + 1] = ((selectColors[color] >> 16) & 0xff) / lowBright;
+            buf[startPos + 2] = (selectColors[color] & 0xff) / lowBright;
         }
         pins.i2cWriteBuffer(RGB_TOUCHKEY_ADDRESS, buf);
     }

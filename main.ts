4781c82857ddd0165de79500beb06217d90d7210
liyunbao <liyunbao33@163.com>
2020-09-18 16:21:33 +0800

    修复bug

--- main.ts
+++ main.ts
@@ -252,7 +252,7 @@ namespace ovobotModules {
     /**
      * TODO: 读取触摸按键。
      */
-    //% blockId=read_touch block="read touch %index data"
+    //% blockId=read_touch block="read %module touch data"
     //% weight=65
     export function readTouchData(module: ModuleIndex): number{
         pins.i2cWriteRegister(SEG_ADDRESS, 0x00, 0x01);

1da3b5a95884a10d9b210124a8081560f006224f
liyunbao <liyunbao33@163.com>
2020-11-09 23:01:11 +0800

    修改

--- main.ts
+++ main.ts
@@ -339,7 +339,7 @@ namespace ovobotModules {
     //% blockId=voice_out block="voice out %sndstr"
     //% weight=65
     export function voiceOut(sndstr: String) {
-        const text = sndstr
+        let text = sndstr
         let buf = pins.createBuffer(256);
         buf[0] = 0xf2;
         buf[1] = 1;
